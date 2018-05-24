c1bbdabae4606027ed88f9effdce40ef99b4bfd6
Trinick <fallonw95@gmail.com>
2018-05-24 01:16:59 +0000

    Removed forum stuff

--- js/OpenForumAPI/Base.js
+++
@@ -1,9 +0,0 @@
-var PostInquiry=require('./PostInquiry.js');
-var PostRequest=require('./PostRequest.js');
-var UserRequest=require('./UserRequest.js');
-
-module.exports = {
- getForumListing: PostInquiry.getForumListing,
- doPostAction: PostRequest.doPostAction,
- doLoginAction: UserRequest.doLoginAction
-};
